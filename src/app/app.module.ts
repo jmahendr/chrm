e29c4ba000f75d4a5061e936979d4cf9cca4ac78
Joshua Mahendran <joshua.mahendran@gmail.com>
2018-02-28 19:11:31 +0530

    Add component Home, Offer. Setup Router for menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { MatListModule, MatButtonModule,MatToolbarModule } from "@angular/material";
-
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { OfferComponent } from './offer/offer.component';
+import { HomeComponent } from './home/home.component';
+
+import { AppRoutingModule } from "./app-routing/app-routing.module";
+
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    OfferComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     MatToolbarModule,
     MatListModule,
-    MatButtonModule
+    MatButtonModule,
+    AppRoutingModule,
+    FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
